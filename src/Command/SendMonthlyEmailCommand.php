d214c6fd78e27e9873ba6a6e1edf8087fe362331
Martyn Wheeler <martyndwheeler@gmail.com>
2022-02-03 17:10:41 +0000

    update cmd

--- src/Command/SendMonthlyEmailCommand.php
+++ src/Command/SendMonthlyEmailCommand.php
@@ -34,24 +34,26 @@ class SendMonthlyEmailCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
 
-        $users = $this->rd->getRideData(year: null, username: null)['users'];
-        //Create a message
-        $io->progressStart();
-        $message = (new Email())
-        ->from(new Address($_ENV['MAILER_FROM'], 'Century Challenge Contact'))
-        ->to($_ENV['MAILER_FROM'])
-        ->subject('Message from Century Challenge')
-        ->text(
-            'Message from: '.$_ENV['MAILER_FROM']."\n\r".'This is a test'
-        );
-        //Add BCC to non-disqualified users
-        $message->addBcc('martyndwheeler@gmail.com');
-        /** @var Symfony\Component\Mailer\SentMessage $sentEmail */
-        $sentEmail = $this->mailer->send($message);
-        $io->progressAdvance();
-        $io->progressFinish();
-
-        $io->success('Monthly email message sent.');
+        if(date('t') == date('d')){
+            $users = $this->rd->getRideData(year: null, username: null)['users'];
+            //Create a message
+            $io->progressStart();
+            $message = (new Email())
+            ->from(new Address($_ENV['MAILER_FROM'], 'Century Challenge Contact'))
+            ->to($_ENV['MAILER_FROM'])
+            ->subject('Message from Century Challenge')
+            ->text(
+                'Message from: '.$_ENV['MAILER_FROM']."\n\r".'This is a test'
+            );
+            //Add BCC to non-disqualified users
+            $message->addBcc('martyndwheeler@gmail.com');
+            /** @var Symfony\Component\Mailer\SentMessage $sentEmail */
+            $sentEmail = $this->mailer->send($message);
+            $io->progressAdvance();
+            $io->progressFinish();
+    
+            $io->success('Monthly email message sent.');
+        }
         return Command::SUCCESS;
     }
 }
