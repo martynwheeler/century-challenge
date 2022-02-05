34ddb6d3094f0b696ce11e63061a1cc28097974f
Martyn Wheeler <martyndwheeler@gmail.com>
2022-02-05 22:56:39 +0000

    do it

--- src/EventListener/HeavyTaskListener.php
+++ src/EventListener/HeavyTaskListener.php
@@ -22,7 +22,45 @@ class HeavyTaskListener
             $request = $event->getRequest();
             $currentRoute = $this->router->match($request->getPathInfo());
             if ('webhook' === $currentRoute['_route']) {
-                
+                /*
+                $aspect_type = $request->get('aspect_type'); // "create" | "update" | "delete"
+                $object_id = $request->get('object_id'); // activity ID | athlete ID
+                $object_type = $request->get('object_type'); // "activity" | "athlete"
+                $owner_id = $request->get('owner_id'); // athlete ID
+        
+                if ($aspect_type == 'create' && $object_type == 'activity') {
+                    //Does ride exist
+                    $entityManager = $this->em->getRepository(Ride::class);
+                    if ($entityManager->findOneBy(['ride_id' => $object_id]) == null){
+                        //Get the user
+                        $entityManager = $this->em->getRepository(User::class);
+                        $user = $entityManager->findOneBy(['stravaID' => $owner_id]);
+                        //Get or refresh token as necessary
+                        if (!$request->getSession()->get('strava.token') || $user->getStravaTokenExpiry() - time() < 300) {
+                            $accessToken = $strava_api->getToken($user);
+                            $request->getSession()->set('strava.token', $accessToken);
+                        }
+                        $token = $request->getSession()->get('strava.token');
+                        $ride = new Ride();
+                        $ride->setUser($user);
+                        $ride->setSource($user->getPreferredProvider());
+        
+                        $athleteActivity = $strava_api->getAthleteActivity($token, $object_id);
+                        if ($athleteActivity) {
+                            $ride->setRideId($object_id);
+                            $ride->setKm($athleteActivity['distance']);
+                            $ride->setAverageSpeed($athleteActivity['average']);
+                            $ride->setDate($athleteActivity['date']);
+                            $ride->setClubRide($strava_api->isClubRide($token, $object_id, $athleteActivity['date']));
+                            if ($strava_api->isRealRide($token, $object_id)){
+                                $entityManager = $this->doctrine->getManager();
+        //                        $entityManager->persist($ride);
+        //                        $entityManager->flush();
+                            }
+                        }
+                    }
+                }
+            */
             }
       }
 }
