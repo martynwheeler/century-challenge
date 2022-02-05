aa54fcf6dde7e9c859a23acf56491bb33aad0a95
Martyn Wheeler <martyndwheeler@gmail.com>
2022-02-05 18:29:13 +0000

    user

--- src/Controller/StravaWebhookController.php
+++ src/Controller/StravaWebhookController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
