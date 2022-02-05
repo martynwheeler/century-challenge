cb4624c08d1f1a6f44b2d304eb956be02cbbab4c
Martyn Wheeler <martyndwheeler@gmail.com>
2022-02-05 17:19:59 +0000

    json

--- src/Controller/StravaWebhookController.php
+++ src/Controller/StravaWebhookController.php
@@ -32,6 +32,7 @@ class StravaWebhookController extends AbstractController
     #[Route('/strava/webhook', name:'webhook', methods: ['POST'])]
     public function data(Request $request): Response
     {
+        $data = $request->request->get('json');
         $aspect_type = $request->request->get('aspect_type'); // "create" | "update" | "delete"
         $object_id = $request->request->get('object_id'); // activity ID | athlete ID
         $object_type = $request->request->get('object_type'); // "activity" | "athlete"
@@ -41,7 +42,7 @@ class StravaWebhookController extends AbstractController
             $messagetousers = $object_id;
         }
 
-        $messagetousers = $request;
+        $messagetousers = $data;
 
         $message = (new Email())
         ->from(new Address($_ENV['MAILER_FROM'], 'Century Challenge Contact'))
