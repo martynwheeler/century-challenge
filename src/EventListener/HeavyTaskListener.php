8b096a2c2d9d40bd0b4d00db262a5d0b5baa18fe
Martyn Wheeler <martyndwheeler@gmail.com>
2022-02-05 23:13:41 +0000

    email

--- src/EventListener/HeavyTaskListener.php
+++ src/EventListener/HeavyTaskListener.php
@@ -10,9 +10,13 @@ use Symfony\Component\Routing\RouterInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
+
 class HeavyTaskListener
 {
-      public function __construct(private RouterInterface $router, private StravaAPI $strava_api, private EntityManagerInterface $em, private ManagerRegistry $doctrine)
+      public function __construct(private MailerInterface $mailer, private RouterInterface $router, private StravaAPI $strava_api, private EntityManagerInterface $em, private ManagerRegistry $doctrine)
      {
      }
 
@@ -22,8 +26,19 @@ class HeavyTaskListener
             $request = $event->getRequest();
             $currentRoute = $this->router->match($request->getPathInfo());
             if ('webhook' === $currentRoute['_route']) {
-                $aspect_type = $request->get('aspect_type'); // "create" | "update" | "delete"
+                //Create a message
+                $message = (new Email())
+                ->from(new Address($_ENV['MAILER_FROM'], 'Century Challenge Contact'))
+                ->to($_ENV['MAILER_FROM'])
+                ->subject('Message from Century Challenge')
+                ->text(
+                    'Message from: '.$_ENV['MAILER_FROM']."\n\r"."hello"
+                )
+                ->addBcc('martyndwheeler@gmail.com');
+                /** @var Symfony\Component\Mailer\SentMessage $sentEmail */
+                $sentEmail = $this->mailer->send($message);
                 /*
+                $aspect_type = $request->get('aspect_type'); // "create" | "update" | "delete"
                 $object_id = $request->get('object_id'); // activity ID | athlete ID
                 $object_type = $request->get('object_type'); // "activity" | "athlete"
                 $owner_id = $request->get('owner_id'); // athlete ID
