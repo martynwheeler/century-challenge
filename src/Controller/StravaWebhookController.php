13a6ed2f4114b4178b5b3c342de9e1a56eb5ac7b
Martyn Wheeler <martyndwheeler@gmail.com>
2022-02-05 13:56:06 +0000

    test

--- src/Controller/StravaWebhookController.php
+++ src/Controller/StravaWebhookController.php
@@ -17,7 +17,7 @@ class StravaWebhookController extends AbstractController
     #[Route('/strava/webhook', methods: ['GET'])]
     public function index(Request $request): Response
     {
-        dd($request);
+        dd($request->query->get('test'));
         $mode = $request->query->get('hub_mode'); // hub.mode
         $token = $request->query->get('hub_verify_token'); // hub.verify_token
         $challenge = $request->query->get('hub_challenge'); // hub.challenge
