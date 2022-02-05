2097935a3e69c4d9e6a6f32e17277cde100cddfb
Martyn Wheeler <martyndwheeler@gmail.com>
2022-02-05 22:50:29 +0000

    Deploy

--- src/Controller/StravaWebhookController.php
+++ src/Controller/StravaWebhookController.php
@@ -12,6 +12,7 @@ class StravaWebhookController extends AbstractController
 {
     public function __construct()
     {
+
     }
 
     #[Route('/strava/webhook', name:'webhook_create', methods: ['GET'])]
