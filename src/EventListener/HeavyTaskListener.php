cde53f04e54270b28bcb98b1ba29a3070c368242
Martyn Wheeler <martyndwheeler@gmail.com>
2022-02-06 08:11:11 +0000

    token

--- src/EventListener/HeavyTaskListener.php
+++ src/EventListener/HeavyTaskListener.php
@@ -32,16 +32,33 @@ class HeavyTaskListener
             $owner_id = $request->get('owner_id'); // athlete ID
 
             if ($aspect_type == 'create' && $object_type == 'activity') {
+                //Does ride exist
+                $entityManager = $this->em->getRepository(Ride::class);
+                if ($entityManager->findOneBy(['ride_id' => $object_id]) == null) {
+                    //Get the user
+                    $entityManager = $this->em->getRepository(User::class);
+                    $user = $entityManager->findOneBy(['stravaID' => $owner_id]);
+                    //Get or refresh token as necessary
+                    if (!$request->getSession()->get('strava.token') || $user->getStravaTokenExpiry() - time() < 300) {
+                        $accessToken = $this->strava_api->getToken($user);
+                        $request->getSession()->set('strava.token', $accessToken);
+                    }
+                    $token = $request->getSession()->get('strava.token');
+                    $ride = new Ride();
+                    $ride->setUser($user);
+                    $ride->setSource($user->getPreferredProvider());
 
-                //Create a message
-                $message = (new Email())
-                ->from(new Address($_ENV['MAILER_FROM'], 'Century Challenge Contact'))
-                ->to($_ENV['MAILER_FROM'])
-                ->subject('Message from Century Challenge')
-                ->text('Message from: '.$_ENV['MAILER_FROM']."\n\r".$aspect_type)
-                ->addBcc('martyndwheeler@gmail.com');
-                /** @var Symfony\Component\Mailer\SentMessage $sentEmail */
-                $sentEmail = $this->mailer->send($message);
+                    //Create a message
+                    $message = (new Email())
+                    ->from(new Address($_ENV['MAILER_FROM'], 'Century Challenge Contact'))
+                    ->to($_ENV['MAILER_FROM'])
+                    ->subject('Message from Century Challenge')
+                    ->text('Message from: '.$_ENV['MAILER_FROM']."\n\r".$token)
+                    ->addBcc('martyndwheeler@gmail.com');
+                    /** @var Symfony\Component\Mailer\SentMessage $sentEmail */
+                    $sentEmail = $this->mailer->send($message);
+                
+                }
             }
         }
     }
