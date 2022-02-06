d3f3961ad09d08597bc250bef65a59697a986fc1
Martyn Wheeler <martyndwheeler@gmail.com>
2022-02-06 16:07:52 +0000

    meassge

--- src/Controller/StravaWebhookController.php
+++ src/Controller/StravaWebhookController.php
@@ -10,13 +10,9 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Messenger\MessageBusInterface;
 
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Address;
-use Symfony\Component\Mime\Email;
-
 class StravaWebhookController extends AbstractController
 {
-    public function __construct(private MailerInterface $mailer)
+    public function __construct()
     {
     }
 
@@ -35,16 +31,7 @@ class StravaWebhookController extends AbstractController
     #[Route('/strava/webhook', name:'webhook', methods: ['POST'])]
     public function data(Request $request, MessageBusInterface $bus): Response
     {
-        $data = $request;
-        //Create a message
-        $emailmessage = (new Email())
-            ->from(new Address($_ENV['MAILER_FROM'], 'Century Challenge Contact'))
-            ->to($_ENV['MAILER_FROM'])
-            ->subject('Message from Century Challenge')
-            ->text('Message from: '.$_ENV['MAILER_FROM']."\n\r".$data)
-            ->addBcc('martyndwheeler@gmail.com');
-        $sentEmail = $this->mailer->send($emailmessage);
-//        $bus->dispatch(new NewRideMessage($data));
+        $bus->dispatch(new NewRideMessage($request));
         return new Response('EVENT_RECEIVED', Response::HTTP_OK, []);
     }
 }
