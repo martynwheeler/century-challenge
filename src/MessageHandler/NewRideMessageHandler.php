c604242e5d9b95b0c79e0454c3e28ff7f3d18949
Martyn Wheeler <martyndwheeler@gmail.com>
2022-02-12 19:31:14 +0000

    Add email to user on strava upload

--- src/MessageHandler/NewRideMessageHandler.php
+++ src/MessageHandler/NewRideMessageHandler.php
@@ -11,15 +11,15 @@ use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 
-//use Symfony\Component\Mailer\MailerInterface;
-//use Symfony\Component\Mime\Address;
-//use Symfony\Component\Mime\Email;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
 
 #[AsMessageHandler]
 class NewRideMessageHandler
 {
     public function __construct(
-//        private MailerInterface $mailer,
+        private MailerInterface $mailer,
         private StravaAPI $strava_api,
         private EntityManagerInterface $em,
         private ManagerRegistry $doctrine,
@@ -71,18 +71,16 @@ class NewRideMessageHandler
                             $entityManager = $this->doctrine->getManager();
                             $entityManager->persist($ride);
                             $entityManager->flush();
+
+                            //emailmessage a message
+                            $emailmessage = (new Email())
+                            ->from(new Address($_ENV['MAILER_FROM'], 'Century Challenge Contact'))
+                            ->to($user->getEmail())
+                            ->subject('Message from Century Challenge')
+                            ->text('Message from: '.$_ENV['MAILER_FROM']."\n\r"."Your ride with id=$object_id has successfully been added.");
+                            $sentEmail = $this->mailer->send($emailmessage);
                         }
                     }
-    /*
-                    //emailmessage a message
-                    $emailmessage = (new Email())
-                    ->from(new Address($_ENV['MAILER_FROM'], 'Century Challenge Contact'))
-                    ->to($_ENV['MAILER_FROM'])
-                    ->subject('Message from Century Challenge')
-                    ->text('Message from: '.$_ENV['MAILER_FROM']."\n\r"."hello")
-                    ->addBcc('martyndwheeler@gmail.com');
-                    $sentEmail = $this->mailer->send($emailmessage);
-    */
                 }
             }
         }
